1ffe5b91c4721992bf67dfc7d7c7d704135d4562
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-08-05 15:17:11 +0300

    ConditionIfElse from Random

---
+++ ConditionIfElse/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter user name");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+
+{
+Console.WriteLine("Cool, it's Маша!");
+}
+
+else 
+
+{
+Console.WriteLine("Hello, ");
+Console.Write(username);
+}
